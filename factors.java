d0236f60dd16ffc43c77e953c885c74288e6f704
DineshKumar H <kumardinesh.hari@gmail.com>
2024-12-20 18:57:30 +0530

    find the factors

---
+++ factors.java
@@ -0,0 +1,22 @@
+class factors
+{
+    public static void main(String[]args){
+        int num = 20;
+
+        for(int i=1;i<=num;i++)
+        {
+            if(num%i==0)
+            System.out.println(i);
+        }
+    }
+}
+
+/*
+1
+2 
+4 
+5 
+10
+20
+
+* /
