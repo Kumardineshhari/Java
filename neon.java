b0703ee6ab178f8395b6e0f886e1871d04e677c9
DineshKumar H <kumardinesh.hari@gmail.com>
2024-12-20 18:45:47 +0530

    check the number is neon or not

---
+++ neon.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class neon{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the value: ");
+        int num = sc.nextInt();
+
+        int square = num*num;
+        int lastdigit = 0;
+        int sum = 0;
+
+        while(square>0){
+            lastdigit=square%10;
+            sum=sum+lastdigit;
+            square=square/10;
+        }
+        if(num==sum)
+            System.out.println("neon number");
+        else
+            System.out.println("not a neon number");
+    }
+}
